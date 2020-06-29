3a91576b87803a71b62a2effe427c849335eea94
blackowe <blackowe@oregonstate.edu>
2020-06-29 18:15:17 -0500

    Project 2a - average

---
+++ average.py
@@ -0,0 +1,8 @@
+print("Please enter five numbers.")     # input prompt for user
+num1 = float(input())                   # saves user input as float X5
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+num_avg = (num1 + num2 + num3 + num4 + num5)/5  # creates average into a variable
+print("The average of those number is:\n",num_avg)  # prints str. with output on new line
